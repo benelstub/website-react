ad5a08a5b53b27870a30d4ea4bacd17cc85f8fde
Ben <43441562+Stopey@users.noreply.github.com>
2023-10-20 12:22:59 +0100

    feat: added about and experience component

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { About } from "../components/global/About";
+import { Experience } from "../components/Experience";
 
 export const Home = () => {
   return (
     <>
-      <h1 className="text-red-500">Home</h1>
+      <section className="p-10 ">
+        <h1 className=" mb-[50px]">About</h1>
+        <About />
+        <Experience />
+      </section>
     </>
   );
 };
